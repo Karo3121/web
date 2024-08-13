80300991820afc08b3de6d1a5b80bbcaf6bc76b5
Karo3121 <148989291+Karo3121@users.noreply.github.com>
2024-08-13 17:41:50 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
     // WEBCAM STREAM TO VIDEO
     let v = document.createElement("video");
+    v.height = "100px"
+    v.width = "100px"
     v.autoplay = true;
     v.srcObject = stream;
    
