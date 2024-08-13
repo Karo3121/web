aec8f735f39f1c44553679886a2e2104cb3b8bec
Karo3121 <148989291+Karo3121@users.noreply.github.com>
2024-08-13 14:28:04 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+const webcamElement = document.getElementById('webcam');
+
+
+navigator.mediaDevices.getUserMedia({video : true}).then((stream) => {
+    webcamElement.srcObject = stream;
+}).catch((err) => {
+    console.error(err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
