a91f54d9fae188e80e77d0126a6d47c758ceee49
Karo3121 <148989291+Karo3121@users.noreply.github.com>
2024-08-13 17:20:28 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,31 @@
 
 
 
+
+
+
 const webcamElement = document.getElementById('webcam');
+const photo = document.getElementById("photo")
+
 
 
 navigator.mediaDevices.getUserMedia({video : true}).then((stream) => {
     
     
     webcamElement.srcObject = stream;
+    
+    const webcam = new Webcam(webcamElement);
 
-
- 
+    photo.src = webcam.snap()
 
 
 }).catch((err) => {
-   
+    console.error(err)
 })
+
+
+
+
+
+
 
