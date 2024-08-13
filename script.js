63989c6750a2d05e14af71ec8228f09484ccc684
Karo3121 <148989291+Karo3121@users.noreply.github.com>
2024-08-13 15:59:37 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,33 @@
 
 
 
+
+
 const webcamElement = document.getElementById('webcam');
 const canvas = document.getElementById('canvas');
+const photo = document.getElementById("photo")
 
 
 navigator.mediaDevices.getUserMedia({video : true}).then((stream) => {
     
     
     webcamElement.srcObject = stream;
-    canvas.getContext('2d').drawImage(webcamElement, 0, 0, webcamElement.width, webcamElement.height);
+
+    canvas.getContext('2d').drawImage(webcamElement, 0, 0, 100, 100);
    
+    const data = canvas.toDataURL("image/png");
+    
+    photo.setAttribute("src", data);
+
+
 }).catch((err) => {
     console.error(err)
 })
+
+
+
+
+
 
 
 
