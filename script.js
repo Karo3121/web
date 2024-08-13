118563d19384c72c699b3c01be63c39547e13753
Karo3121 <148989291+Karo3121@users.noreply.github.com>
2024-08-13 17:01:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 
+import Webcam from 'webcam-easy';
 
 
 const webcamElement = document.getElementById('webcam');
-const photo = document.getElementById("photo")
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
 
 
-
-navigator.mediaDevices.getUserMedia({video : true}).then((stream) => {
-    
-    
-    webcamElement.srcObject = stream.snap();
-
-
-
-}).catch((err) => {
-    console.error(err)
-})
+webcam.start()
+   .then(result =>{
+      console.log("webcam started");
+   })
+   .catch(err => {
+       console.log(err);
+   });
 
 
+const photo = document.getElementById('photo')
 
 
+var picture = webcam.snap();
 
-
-
-
-
-
-
-
+photo.src = picture
 
 
